c2d214735167fb84a7bc4ed6a5e26e26f7e6a2f1
Vladislav Shesternev <vladislavshesternev1@gmail.com>
2023-03-02 18:39:56 +0200

    Configured models for the admin panel

--- papertrail/books/admin.py
+++ papertrail/books/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from .models import (
+    Category,
+    Book,
+    Author,
+    Publisher,
+    Series,
+    Interpreter,
+    Illustrator,
+    Language,
+    Binding,
+    Illustrations,
+    Font,
+    Review
+)
 
-# Register your models here.
+admin.site.register(Language)
+admin.site.register(Binding)
+admin.site.register(Illustrations)
+admin.site.register(Font)
+admin.site.register(Review)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Interpreter)
+class InterpreterAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Illustrator)
+class IllustratorAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
